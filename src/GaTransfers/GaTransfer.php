45d283e16023dbc6a51787f2f59494cc939b40dd
Kim Tung <nguyenkim08@gmail.com>
2020-02-26 09:13:11 +0700

    feature: init package

---
+++ src/GaTransfers/GaTransfer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KimTung
+ * Date: 2/26/2020
+ * Time: 9:08 AM
+ */
+
+namespace GaTransfers;
+
+
+class GaTransfer
+{
+    public function __construct() {
+
+    }
+}
