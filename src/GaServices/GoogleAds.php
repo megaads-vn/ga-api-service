571978828c8e9f187e666c0673381b19ca09db9c
Kim Tung <nguyenkim08@gmail.com>
2020-02-26 11:19:03 +0700

    feature: init google ads service

---
+++ src/GaServices/GoogleAds.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KimTung
+ * Date: 2/26/2020
+ * Time: 11:03 AM
+ */
+
+namespace GaServices;
+
+
+class GoogleAds
+{
+    public function __construct()
+    {
+
+    }
+
+
+}
---
+++ src/GaServices/Service/AdwordsService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KimTung
+ * Date: 2/26/2020
+ * Time: 11:16 AM
+ */
+
+namespace GaServices\Service;
+
+
+class AdwordsService
+{
+
+}
